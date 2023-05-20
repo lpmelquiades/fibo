448de54e14f9c37cfe14661556e7abe897d82bae
Lucas Melquiades <lmelquiades@primeit.pt>
2023-05-20 16:05:06 -0300

    supports 404 error

---
+++ src/App/HttpCode.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+enum HttpCode: int {
+    case NOT_FOUND_404 = 404;
+}
--- src/App/webserver.php
+++ src/App/webserver.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Dependency;
+use App\NotFoundExceptionCatch;
 use Fibonacci\Input\FibonacciAction;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
@@ -8,10 +9,8 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 $dependency = new Dependency();
 $app = \DI\Bridge\Slim\Bridge::create($dependency->container);
 $app->addRoutingMiddleware();
-$app->addErrorMiddleware(true, true, true);
 
-
-// $app->add(NotFoundExceptionCatch::class);
+$app->add(NotFoundExceptionCatch::class);
 // $app->add(RequestResponseLogging::class);
 
 $app->get('/fibonacci/{count}', FibonacciAction::class);
